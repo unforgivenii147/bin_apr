fd3bee7d51da1df62ac42e5730e16b99112aa6b3
unforgivenii147 <adnanonagh@gmail.com>
2026-05-16 00:55:59 +0430

    2026-05-16 00:55:59

--- top500.py
+++ top500.py
@@ -1,3 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/python
+
     DIRECTORY,
     CHUNK_SIZE,
     non_english_pattern,
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/python
+
 DIRECTORY = "."
 
 CHUNK_SIZE = 2000
